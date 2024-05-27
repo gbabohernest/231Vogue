8bcd81145f474ceb79573719f3303fafdfc1b9ea
gbabohernest <gbaboh.ernest@gmail.com>
2024-05-27 09:32:17 +0300

    Listens and handle mouseenter and mouseleave events on our CTA dropdownmenu

--- public/assets/js/sandbox.js
+++ public/assets/js/sandbox.js
@@ -4,6 +4,13 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    showCategoriesDropdownMenu();
+    accountCTADropdownMenu();
+});
+
+
+const showCategoriesDropdownMenu = () => {
     const dropdown = document.querySelector('#dropdown');
     const dropdownMenu = dropdown.querySelector('#dropdown-menu');
     const chevron = dropdown.querySelector('.chevron');
@@ -36,16 +43,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 200);
     });
 
-
-    accountCTADropdownMenu();
-});
-
-
+}
 const accountCTADropdownMenu = () => {
     const accountCtaDropdown = document.querySelector('#account-cta-dropdown');
     const ctaDropdownBtn = accountCtaDropdown.querySelector(".account-cta--dropdown");
     const chevron = ctaDropdownBtn.querySelector('.chevron');
     const ctaDropdownMenu = accountCtaDropdown.querySelector('.account-cta-wrapper');
+
+    let timeout;
 
     ctaDropdownBtn.addEventListener('click', (e) => {
         e.stopPropagation();
@@ -53,18 +58,47 @@ const accountCTADropdownMenu = () => {
         ctaDropdownMenu.classList.toggle('show-cta-dropdown');
     });
 
+    accountCtaDropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeout);
+    })
+
+    accountCtaDropdown.addEventListener('mouseleave', () => {
+        timeout = setTimeout(() => {
+            if (ctaDropdownMenu.classList.contains('show-cta-dropdown') && chevron.classList.contains('rotate')) {
+                ctaDropdownMenu.classList.remove('show-cta-dropdown');
+                chevron.classList.remove('rotate');
+            }
+        }, 200)
+    });
+
+
+    ctaDropdownMenu.addEventListener('mouseenter', () => {
+        clearTimeout(timeout);
+    })
+
+    ctaDropdownMenu.addEventListener('mouseleave', () => {
+        timeout.setTimeout(() => {
+            if (ctaDropdownMenu.classList.contains('show-cta-dropdown') && chevron.classList.contains('rotate')) {
+                ctaDropdownMenu.classList.remove('show-cta-dropdown');
+                chevron.classList.remove('rotate');
+            }
+        }, 200)
+    })
 
     // hide when clicking outside
     document.addEventListener('click', () => {
         if (ctaDropdownMenu.classList.contains('show-cta-dropdown')) {
             ctaDropdownMenu.classList.remove('show-cta-dropdown');
+            chevron.classList.remove('rotate');
         }
     });
 
 
     ctaDropdownMenu.addEventListener('click', (e) => {
         e.stopPropagation();
-    })
-
+    });
 
 }
+
+
+
