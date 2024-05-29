0177a5b9376670a577b767ec68a3cbe8b8115fa2
gbabohernest <gbaboh.ernest@gmail.com>
2024-05-29 19:44:49 +0300

    admin dashboard sidebar navigation script

---
+++ public/assets/js/admin.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const openSideBarBtn = document.querySelector('.admin-toggle-open');
+    const closeSideBarBtn = document.querySelector('.admin-toggle-close');
+    const sideBar = document.querySelector('.admin__container--sidebar');
+    const adminMainContent = document.querySelector('.admin__container--main-content');
+
+
+    openSideBarBtn.addEventListener('click', () => {
+
+
+        if (closeSideBarBtn.classList.contains('hide-admin-element')) {
+            closeSideBarBtn.classList.remove('hide-admin-element');
+        }
+
+        if (sideBar.classList.contains('hide-admin-sidebar') && !adminMainContent.classList.contains('hide-admin-element')) {
+            adminMainContent.classList.add('hide-admin-element');
+            sideBar.classList.remove('hide-admin-sidebar');
+            sideBar.style.flexGrow = '1';
+        }
+
+        openSideBarBtn.classList.add('hide-admin-element');
+    });
+
+    closeSideBarBtn.addEventListener('click', () => {
+
+        if (openSideBarBtn.classList.contains('hide-admin-element')) {
+            openSideBarBtn.classList.toggle('hide-admin-element');
+        }
+
+        if (!sideBar.classList.contains('hide-admin-sidebar') && adminMainContent.classList.contains('hide-admin-element')) {
+            adminMainContent.classList.toggle('hide-admin-element');
+            sideBar.classList.toggle('hide-admin-sidebar');
+            sideBar.style.flexGrow = '0';
+
+        }
+
+        closeSideBarBtn.classList.toggle('hide-admin-element');
+    })
+})
+
