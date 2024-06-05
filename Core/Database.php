b3a0c9e947f3b78f581b23e912c7062a49ea192e
gbabohernest <gbaboh.ernest@gmail.com>
2024-06-06 00:08:47 +0300

    Update Database class, add the Database class to the Core namespace for easy usage of the class and to keep the code organized. Update the query method to return the database instance. Add a method get, that fetches all the records of a given query from the database, also add a method find and find_or_fail - both fetches a single record from the database for a given query, the latter handles the case where getting the data was unsuccessful, it calls the abort function and give it a response code to notify the user of what has happened.

--- Core/Database.php
+++ Core/Database.php
@@ -4,10 +4,19 @@
  * Manages database connection and interactions
  */
 
+namespace Core;
+
+use Exception;
+use PDO;
+use PDOException;
+use PDOStatement;
+
 class Database
 {
 
     public PDO $connection_db;
+    public PDOStatement $statement;
+
 
     public function __construct(array $config, array $options)
     {
@@ -31,13 +40,53 @@ class Database
 
     }
 
-    public function query($query, $params = []): false|PDOStatement
+    public function query($query, $params = []): static
     {
-        $statement = $this->connection_db->prepare($query);
+        $this->statement = $this->connection_db->prepare($query);
 
-        $statement->execute($params);
+        $this->statement->execute($params);
 
-
-        return $statement;
+        return $this;
     }
+
+    /**
+     * Fetch a single record from the database
+     * @return mixed
+     */
+    public function find(): mixed
+    {
+
+        return $this->statement->fetch();
+    }
+
+    /**
+     * Fetch a single record upon success or handle failure
+     * @throws Exception
+     */
+    public function find_or_fail()
+    {
+        $result = $this->find();
+
+        if (!$result) {
+            abort(Response::INTERNAL_SERVER_ERROR);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Fetch all the records from the database for a particular query.
+     * @throws Exception
+     */
+    public function get(): false|array
+    {
+        $results = $this->statement->fetchAll();
+
+        if (!$results) {
+            abort(Response::INTERNAL_SERVER_ERROR);
+        }
+
+        return $results;
+    }
+
 }
