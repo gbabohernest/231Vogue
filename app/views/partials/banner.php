d6a2be96aeead2939e4ceae27045c8e0bfb841e4
gbabohernest <gbaboh.ernest@gmail.com>
2024-06-05 19:26:29 +0300

    231vogue banner that display our models, CTA  shop now button for quick shopping and controls for switching between the different models and background-color been shown in the slideshow

---
+++ app/views/partials/banner.php
@@ -0,0 +1,56 @@
+<section class="banner">
+
+    <div class="banner__container">
+        <header class="banner__container-header">
+            <div class="banner__container-header--text">
+                <h2>
+                    <span class="pop">makeover</span>
+                    <br>
+                    <span class="cursive">Your Fashion</span>
+                    <br>
+                    <span class="pop">Look</span>
+                </h2>
+            </div>
+            <p class="banner__text-content__header__content__description">
+                Your one-stop shop for all Ladies' fashion. <br>Meet our new autumn collection from <br>famous brands
+                and young designers - exclusively at Fabulous!
+            </p>
+            <div class="banner__container-header--shop-now-btn">
+                <!--  hide on mobile                 -->
+                <a href="#">Shop now</a>
+            </div>
+
+        </header>
+
+        <div class="banner__container__slide-show-wrapper">
+
+            <img src="/" class="model-pic" alt="">
+            <div class="slide-show-control">
+                <button class="fa-solid fa-left-long prev banner-prev"></button>
+                <button class="fa-solid fa-right-long next banner-next"></button>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="header_footer">
+    <div class="header_footer__wrapper">
+        <div class="header_footer__wrapper--card">
+            <i class="fa-solid fa-truck"></i>
+            <p>Delivery of goods to the next day to your home or office</p>
+        </div>
+
+        <div class="header_footer__wrapper--card">
+
+            <i class="fa-solid fa-credit-card"></i>
+            <p>Convenient payment by bank cards, cash and credit</p>
+        </div>
+
+        <div class="header_footer__wrapper--card">
+            <i class="fa-solid fa-gift"></i>
+            <p>Choose a gift and get a discounts just - a gift card!</p>
+        </div>
+    </div>
+
+</section>
