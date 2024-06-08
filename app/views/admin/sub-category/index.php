1cf28cd3ae6583c87648025bc5ec48ba84c376c5
gbabohernest <gbaboh.ernest@gmail.com>
2024-06-08 23:14:15 +0300

    Loop the list of sub-categories send by the controller the sub-categories view, gather data and then display it in the admin dashboard section of sub-categories for data managment

---
+++ app/views/admin/sub-category/index.php
@@ -0,0 +1,97 @@
+<?php view('partials/head.php'); ?>
+
+    <section class="admin">
+
+        <!-- Admin Navigation Here-->
+        <?php view('partials/admin-partials/nav.php') ?>
+
+        <div class="admin__container">
+            <!-- Sidebar Navigation Here -->
+
+            <?php view('partials/admin-partials/sidebar.php'); ?>
+
+
+            <main class="admin__container--main-content">
+                <header class="page-header">
+                    <h1>Sub-Categories</h1>
+                    <div class="divider"></div>
+                </header>
+
+                <section class="record-section">
+                    <div class="section-header">
+                        <h2>Sub-Categories list</h2>
+                        <div class="section-actions">
+                            <button class="filter-btn"><i class="fa-solid fa-filter"></i>Filter</button>
+                            <button class="see-all-btn">See All</button>
+                            <button class="add-record-btn" data-modal-type="sub-category"><i
+                                        class="fa-solid fa-plus"></i>Add Sub-Category
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="divider"></div>
+
+
+                    <table class="record-table">
+                        <thead>
+                        <tr>
+                            <th>Sub-category Name</th>
+                            <th>Description</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th class="center-th">Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+
+                        <?php foreach ($sub_categories as $sub_category) : ?>
+                            <tr>
+
+                                <td><?= $sub_category['sub_category_name'] ?></td>
+                                <td><?= $sub_category['description'] ?></td>
+                                <td><?= $sub_category['category_name'] ?></td>
+                                <td><?php if ($sub_category['is_active']) : ?>
+                                        <?= "<span class='active-column'>Active</span> " ?>
+                                    <?php else : ?>
+                                        <?= "<span class='not-active-column'>Not Active</span> " ?>
+                                    <?php endif; ?>
+                                </td>
+
+                                <td>
+                                    <button class="edit-btn" data-modal-type="sub-category">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                        Edit
+                                    </button>
+                                    <button class="delete-btn" data-modal-type="sub-category">
+                                        <i class="fa-solid fa-trash-can"></i>
+                                        Delete
+                                    </button>
+                                </td>
+
+                            </tr>
+                        <?php endforeach; ?>
+
+                        </tbody>
+                    </table>
+
+                    <!-- pagination buttons -->
+                    <div class="pagination">
+                        <button><i class="fa-solid fa-left-long prev"></i>Previous</button>
+                        <button>Next <i class="fa-solid fa-right-long next"></i></button>
+                    </div>
+                </section>
+
+                <!-- A model for creating category -->
+                <?php view('admin/sub-category/create.view.php'); ?>
+
+                <!-- A model for editing category -->
+                <?php view('admin/sub-category/edit.view.php'); ?>
+            </main>
+
+
+        </div>
+
+    </section>
+
+<?php view('partials/admin-partials/layouts/footer.php') ?>
