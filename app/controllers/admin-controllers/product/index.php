ba2e7dc863f3c7d8ea56feb88dccf464881a67d6
gbabohernest <gbaboh.ernest@gmail.com>
2024-06-09 15:53:21 +0300

    add a class to the dynamically loading product image to control of how it will be styled

--- app/controllers/admin-controllers/product/index.php
+++ app/controllers/admin-controllers/product/index.php
@@ -12,7 +12,7 @@ $db = new Database($config['database'], [
 
 // query db
 try {
-    $products = $db->query('SELECT p.product_id, p.product_name, p.price, p.description, p.image, p.status, p.created_at, sc.sub_category_name from products p
+    $products = $db->query('SELECT p.product_id, p.product_name, p.price, p.description, p.image_path, p.status, p.created_at, sc.sub_category_name from products p
             JOIN `231vogue_db`.sub_categories sc on p.sub_category_id = sc.sub_category_id order by p.product_id desc ')->get();
 
 } catch (Exception $e) {
