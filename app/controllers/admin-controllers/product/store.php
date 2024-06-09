65eaf26105e44b7086091b943d4a2b6e8659d610
gbabohernest <gbaboh.ernest@gmail.com>
2024-06-09 15:48:30 +0300

    add file upload function to handle the uploading of images, which we can associate with products. The script validate the image file and upload it to a designated directory on the server, a unique file name is generated to avoid file conflicts and then store the file path within a separate column in the product table for ensuring efficient data retrieval and display. I also  did some refactoring. created a dedicated function for validating product data and an array of errors if there were any

--- app/controllers/admin-controllers/product/store.php
+++ app/controllers/admin-controllers/product/store.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Core\Database;
 use Core\Validator;
 
@@ -13,11 +12,13 @@ $db = new Database($config['database'], [
 $errors = [];
 $selected_category = '';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//    dd($_POST);
-
-    $selected_category = $_POST['category'];
-
+/**
+ * Validate product data.
+ * @param array $errors
+ * @return array
+ */
+function validateProductData(array $errors): array
+{
     if (!Validator::string($_POST['name'], 3, 20)) {
 
         $errors['product_name'] = 'Name must be at least 3 chars and not more than 20 chars';
@@ -38,6 +39,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!Validator::string($_POST['status'])) {
         $errors ['status'] = 'A status is required';
     }
+    return $errors;
+}
+
+/**
+ * Handle file upload
+ * @param mixed $image
+ * @return string
+ * @throws Exception
+ */
+function handleFileUpload(mixed $image): string
+{
+    $allowed_types = ['image/jpeg', 'image/png', 'image/gif'];
+
+    if (!in_array($image['type'], $allowed_types)) {
+        throw new Exception('Sorry, File format Not supported');
+    }
+
+    $upload_dir = 'uploads/';
+    if (!is_dir($upload_dir)) {
+        mkdir($upload_dir, 0777, true);
+    }
+
+    $file_extension = pathinfo($image['name'], PATHINFO_EXTENSION);
+    $file_path = $upload_dir . uniqid() . '.' . $file_extension;
+
+    if (!move_uploaded_file($image['tmp_name'], $file_path)) {
+        throw new Exception('Failed to move uploaded file');
+    }
+    return $file_path;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//    dd($_FILES);
+
+    $selected_category = $_POST['category'];
+
+    /**
+     * Validate product data.
+     *
+     * @param array $data
+     * @return array
+     */
+
+    $errors = validateProductData($errors);
 
 
     if (!empty($errors)) {
@@ -53,20 +99,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $status = $_POST['status'];
         $status_value = ($status === 'active') ? 1 : 0;
 
+        $image = $_FILES['image'];
+
         try {
-            $db->query('INSERT INTO products(product_name, price, description, image, sub_category_id, status) Values(
-                          :name, :price, :desc, :image, :sub_category_id, :status)', [
+
+            $file_path = handleFileUpload($image);
+
+        } catch (Exception $e) {
+
+            echo "Error Uploading the file" . $e->getMessage();
+        }
+
+
+        try {
+            $db->query('INSERT INTO products(product_name, price, description, sub_category_id, status, image_path) Values(
+                          :name, :price, :desc,:sub_category_id, :status, :image_path)', [
                 'name' => $_POST['name'],
                 'price' => $_POST['price'],
                 'desc' => $_POST['description'],
-                'image' => $_POST['image'],
                 'sub_category_id' => $selected_category,
                 'status' => $status_value,
+                'image_path' => $file_path
             ]);
 
 
             echo "<script>alert('Data was inserted successfully!!!')</script>";
+
             header('location: /dashboard/products');
+
             exit();
 
         } catch (Exception $e) {
@@ -75,8 +135,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     }
 
-
 }
-
-
-
