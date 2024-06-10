d68b724396a8d920a244b714554d556c5d9310d7
gbabohernest <gbaboh.ernest@gmail.com>
2024-06-10 12:11:56 +0300

    deletes a specific category from the database with the given id which is gathered from the POST super globals variable upon the submission of the delete request

---
+++ app/controllers/admin-controllers/category/destroy.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database'], [
+    'password' => DB_PASSWORD
+]);
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//    dd($_POST);
+
+    if ($_POST['id'] && is_numeric($_POST['id']) !== null) {
+        try {
+            $db->query('delete from categories where category_id = :id', [
+                'id' => $_POST['id']
+            ]);
+
+            header('Location: /dashboard/categories');
+            exit();
+
+        } catch (Exception $e) {
+            echo "Sorry, Unable to delete the record" . $e->getMessage();
+        }
+    }
+}
+
