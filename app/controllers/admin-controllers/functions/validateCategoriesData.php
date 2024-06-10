b0c6295a794174fd8d32f229540e24a59ec42829
gbabohernest <gbaboh.ernest@gmail.com>
2024-06-10 04:44:32 +0300

    validate category inputs and provide feedbacks

---
+++ app/controllers/admin-controllers/functions/validateCategoriesData.php
@@ -0,0 +1,29 @@
+<?php
+
+use Core\Validator;
+
+
+/**
+ * Validate Category data.
+ * @param array $data
+ * @return array
+ */
+function validateCategoryData(array $data): array
+{
+    $errors = [];
+    if (!Validator::string($data['name'], 3, 20)) {
+
+        $errors['category_name'] = 'Name must be at least 3 chars and not more than 20 chars';
+    }
+
+
+    if (!Validator::string($data['description'], 10, 40)) {
+        $errors ['description'] = 'A category description is required, not less than 10 letter or more than 30 letters';
+    }
+
+    if (!Validator::string($data['status'])) {
+        $errors ['status'] = 'A status is required';
+    }
+
+    return $errors;
+}
