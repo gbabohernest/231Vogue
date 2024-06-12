59ebef6dffff1f1da584c19357efd1676c0027ce
gbabohernest <gbaboh.ernest@gmail.com>
2024-06-12 07:34:14 +0300

    functions to log in & log out users, handles session and cookie data

--- Core/functions.php
+++ Core/functions.php
@@ -1,15 +1,17 @@
 <?php
 
+use Core\Response;
+use JetBrains\PhpStorm\NoReturn;
+
 //For debugging delete later
-function dd($value) {
+function dd($value)
+{
     echo '<pre>';
     var_dump($value);
     echo '</pre>';
 
     die();
 }
-
-
 
 /**
  * Returns a path from the project root
@@ -19,21 +21,32 @@ function dd($value) {
 function base_path(string $path): string
 {
 
-    return BASE_PATH . $path;
+    return BASE_PATH . DIRECTORY_SEPARATOR . $path;
 
 }
 
+/**
+ * Require a view within the specific script it was called.
+ * @param array $attributes - values that are need for the view to work with.
+ * @param $path - path to the required view.
+ */
+function view(string $path, array $attributes = []): void
+{
+    extract($attributes);
+
+    require base_path('app/views/' . $path);
+}
 
 /**
- * Return the path to a specific view
- * @param $path - file path.
- * @return string - full file path.
+ * Require a reusable admin function
+ * @param string $path - path to admin reusable functions
+ * @return void
  */
-function view($path): string
+
+function adminFunctions(string $path): void
 {
 
-     return base_path('/app/views/' . $path);
-
+    require base_path('app/controllers/admin-controllers/functions/' . $path);
 }
 
 /**
@@ -47,3 +60,45 @@ function activeURL(string $urlPath): bool
     return $_SERVER['REQUEST_URI'] === $urlPath;
 }
 
+/**
+ * Logs a user in and add the key `user` or `admin` to the session and delete old session data
+ * @param array $user
+ * @return void
+ */
+function logUserIn(array $user): void
+{
+    $_SESSION['user'] = [
+        'name' => htmlspecialchars($user['first_name']),
+        'is_admin' => $user['is_admin'] ?? false,
+    ];
+
+    if ($user['is_admin']) {
+        $_SESSION['admin'] = [
+            'admin_id' => $user['user_id'],
+            'admin_name' => $user['first_name'],
+        ];
+    }
+
+    session_regenerate_id(true);
+}
+
+/**
+ * Logs out a user and clear the session, destroy the session
+ * @return void
+ */
+function logUserOut(): void
+{
+    $_SESSION = [];
+    session_destroy();
+
+    $params = session_get_cookie_params();
+
+    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+}
+
+#[NoReturn] function abort($status_code = Response::NOT_FOUND): void
+{
+    http_response_code($status_code);
+    view("" . (Response::NOT_FOUND) . ".php");
+    die();
+}
